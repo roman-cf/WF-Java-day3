57e866a14acf27226cedde6b5fcdeb4bc3f92b1e
roman-cf <roman.springer@outlook.com>
2019-11-20 09:34:35 +0100

    start

---
+++ src/javaDay3.java
@@ -0,0 +1,13 @@
+public class javaDay3 {
+    public static void main(String[] args) {
+    // A1
+    String hannah = "Did Hannah see bees? Hannah did.";
+        System.out.println(hannah.length());
+        System.out.println(hannah.charAt(12));
+        System.out.println(hannah.indexOf("b"));
+    // A2
+
+
+
+    }
+}

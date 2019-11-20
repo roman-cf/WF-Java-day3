179ef641f0f081c77c14201f35462e20eb7b33aa
roman-cf <roman.springer@outlook.com>
2019-11-20 11:21:23 +0100

    bis 6

--- src/javaDay3.java
+++ src/javaDay3.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.lang.Math.*;
+
 public class javaDay3 {
     public static void main(String[] args) {
     // A1
@@ -21,9 +24,50 @@ public class javaDay3 {
         System.out.println(a3string.replace("dogs","cats"));
         System.out.println(a3string.replaceAll("e","3"));
 
-        String[] output = a3string.split("d");
-        System.out.println(output);
+        String[] output = a3string.split(" ");
+        for (int i = 0; i < output.length;i++){
+            System.out.println(output[i]);
+        }
+
+    // A4
+    String names = "John Doe";
+        String namestring1 = String.valueOf(names.charAt(0));
+        String namestring2 = String.valueOf(names.charAt(5));
+        System.out.println(namestring1+namestring2);
+
+    // A5
+        Scanner a5 = new Scanner(System.in);
+        System.out.print("Enter Temperature in Fahrenheit: ");
+        double fahrenheitValue = Integer.parseInt(a5.nextLine());
+        double celsiusValue = (fahrenheitValue-32)*5/9;
+        System.out.println(celsiusValue);
+
+        Scanner a51 = new Scanner(System.in);
+        System.out.print("Enter Temperature in Celsius: ");
+        double celsiusValue1 = Integer.parseInt(a51.nextLine());
+        double fahrenheitValue1 = (celsiusValue1*1.8)+32;
+        System.out.println(fahrenheitValue1);
+
+    // A6
+        Scanner s6 = new Scanner(System.in);
+        System.out.print("Enter Radius of Circle: ");
+        int radius = Integer.parseInt(s6.nextLine());
+        double area = Math.PI * Math.pow(radius, 2.0);
+        System.out.println("the area of the circle ist: "+ area);
+
+    // A7
+
+
+
+
+
 
 
     }
 }
+public MyClass {
+public static int calcAverage ( int input1, int input2, int input3){
+        System.out.print(input1);
+        System.out.print(input2);
+        }
+        }
